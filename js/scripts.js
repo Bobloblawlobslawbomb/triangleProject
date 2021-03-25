ddd26ab967f0424572beaa03b7032eada99bd0b4
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-03-25 15:40:37 -0700

    finalize the business logic of project
    Patrick Lee <patricklee1138@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,36 @@
-/**/
 function isTriangle() {
-  let sideA = 3;
+  let sideA = 5;
   let sideB = 4;
-  let sideC = 5;
-  if (sideA + sideB > sideC && sideB + sideC > sideA && sideC + sideA > sideB) {
-    alert("then it IS a triangle")//then it IS a triangle
-  } else {
-    alert("it NOT a triangle")// then it is NOT a triangle
+  let sideC = 3;
+  if (sideA <= 0 || sideB <= 0 || sideC <= 0) {
+    alert("please enter valid integers to properly evaluate")
+    //more code to allow user to resubmit
+  }
+  else if (sideA + sideB <= sideC || sideB + sideC <= sideA || sideC + sideA <= sideB) {
+    alert("NOT a triangle");
+  }
+  else if (sideA === sideB && sideA === sideC) {
+    alert("equilateral")
+  } else if (sideA != sideB && sideA === sideC) {
+    alert("isosceles, B sucks")
+  } else if (sideA != sideB && sideB === sideC) {
+    alert("isosceles, A sucks")
+  } else if (sideB != sideC && sideA === sideB) {
+    alert("isosceles, C sucks")
+  }
+  else if (sideA != sideB && sideB != sideC && sideA != sideC) {
+    alert("Scalene triangle")//isoceles or equilateral or scalene
+  }
+  /*else if (sideA + sideB > sideC && sideB + sideC > sideA && sideC + sideA > sideB) {
+    alert("JUST a triangle") //is a triangle it seems this is inneccessary as we have already defined "triangles" with 0,1,2,and 3 of the same length sides
+  }*/
+
+  else {
+
+    alert("dunno how we got here") //catch bad values
   }
 }
-isTriangle()
-
-
+isTriangle() //to test the function
 
 /**/
 
@@ -23,13 +42,5 @@ isTriangle()
 
 
 /* user inputs triangle sides*/
-
-
-if (a + b <= c) && (b + c <= a) && (c + a <= b) {
-  //then its NOT a triangle
-} else {
-  // then it IS a triangle
-}
-
 
 
