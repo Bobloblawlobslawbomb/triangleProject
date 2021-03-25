3d661742d846624473c4c5079aa89bcbdf2d164a
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-03-25 11:57:05 -0700

    begin our triangleProject has working trianglechecker in scripts.js
    Co-authored-by: Patrick Lee <patricklee1138@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+/**/
+function isTriangle() {
+  let sideA = 3;
+  let sideB = 4;
+  let sideC = 5;
+  if (sideA + sideB > sideC && sideB + sideC > sideA && sideC + sideA > sideB) {
+    alert("then it IS a triangle")//then it IS a triangle
+  } else {
+    alert("it NOT a triangle")// then it is NOT a triangle
+  }
+}
+isTriangle()
+
+
+
+/**/
+
+/*(*a*a)+(b*b)=(c*c)*/
+
+
+
+/*user interface logic*/
+
+
+/* user inputs triangle sides*/
+
+
+if (a + b <= c) && (b + c <= a) && (c + a <= b) {
+  //then its NOT a triangle
+} else {
+  // then it IS a triangle
+}
+
+
+
